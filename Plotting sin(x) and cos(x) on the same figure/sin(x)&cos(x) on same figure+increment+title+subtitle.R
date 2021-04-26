abf7a27b0b6b27f593bdebfc6dbd29eb0f11e687
Ameesha Gireesh <67223373+ameeshagireesh@users.noreply.github.com>
2021-04-26 10:18:10 +0400

    Add files via upload
    Let x be from -pi to pi with increment 0.1. Plot sin(x) and cos(x) on the same figure window.
    
    Label main title with your name "Ameesha Gireesh Kumar" and subtitle with your Reg.no "20BCB0107"

---
+++ Plotting sin(x) and cos(x) on the same figure/sin(x)&cos(x) on same figure+increment+title+subtitle.R
@@ -0,0 +1,5 @@
+x = seq(-pi,pi,0.1)
+plot(x,sin(x), main="Ameesha Gireesh Kumar", type="l", col="blue", sub="20BCB0107")
+
+lines(x,cos(x),col="red")
+legend("topleft",c("sin(x)","cos(x)"),fill=c("blue","red"))
